31438cdae11e50830e48b02d98405f40d18d34c6
Daniel Weber <dantech88@yahoo.com>
2018-11-16 02:01:35 -0500

    refactored setup script

--- setup.py
+++ setup.py
@@ -1,121 +1,147 @@
 import serial
+import re
+import glob
+import threading
 
-def write_data(data, linkID=None):
-	ser.flushInput()
-	if linkID != None:
-		ser.write(("AT+CIPSEND=" + str(linkID) + "," + str(len(data)) + "\r\n").encode())
-	else:
-		ser.write(("AT+CIPSEND=" + str(len(data)) + "\r\n").encode())
-	while(1):
-		ret = ser.readline()
-		print("Command result: %s" % ret)
-		if ("OK" in ret.decode()):
-			break
-		if ("ERROR" in ret.decode()):
-			exit()
-	ser.write((data).encode())
-	while(1):
-		ret = ser.readline()
-		print("Command result: %s" % ret)
-		if ("OK" in ret.decode()):
-			break
-		if ("ERROR" in ret.decode()):
-			exit()
+espPat = re.compile("ESP8266-Mesh-\d*")
 
-def write_cmd(cmd):
-	ser.flushInput()
-	ser.write((cmd + "\r\n").encode())
-	while(1):
-		ret = ser.readline()
-		print("Command result: %s" % ret)
-		try:
+class WifiModule(threading.Thread):
+	def __init__(self, port, id, baud="115200"):
+		self.port = port
+		self.id = id
+		ser = serial.Serial(port,baud)
+		if ser.isOpen():
+			ser.close()
+
+		ser.open()
+		ser.isOpen()
+
+		self.ser = ser
+
+		# mapping from ssid to metadata tuple
+		self.direct_conns = {}
+
+		self.connected_to_ap = False
+
+	# TODO add mac address
+	def add_conn(self, linkId, ssid, IP):
+		self.direct_conns[ssid] = (linkID, ssid, IP)
+
+	def setup(self):
+		self.write_cmd("AT")
+		self.write_cmd("AT")
+		# get info
+		self.write_cmd("AT+GMR")
+		# switch to station+AP mode
+		self.write_cmd("AT+CWMODE=3")
+		# set ssid (name), pwd, chnl, enc
+		self.write_cmd("AT+CWSAP_CUR=\"ESP8266-Mesh-{}\",\"1234567890\",5,3".format(self.id))
+		# set ip address to avoid conflicts
+		write_cmd("AT+CIPAP_CUR=\"192.168.{}.1\",\"192.168.{}.1\",\"255.255.255.0\"".format(self.id, self.id))
+		# allow multiple connections (necessary for tcp server)
+		self.write_cmd("AT+CIPMUX=1")
+		# passive mode tcp receive (explicit call)
+		self.write_cmd("AT+CIPRECVMODE=1")
+		# create a tcp server on port 80
+		self.write_cmd("AT+CIPSERVER=1,80")
+		# get local ip
+		self.write_cmd("AT+CIFSR")
+
+	def write_data(self, data, linkID=None):
+		ser = self.ser
+
+		ser.flushInput()
+		if linkID != None:
+			self.write_cmd("AT+CIPSEND=" + str(linkID) + "," + str(len(data)) + "\r\n")
+		else:
+			self.write_cmd("AT+CIPSEND=" + str(len(data)) + "\r\n")
+
+		ser.write((data).encode())
+		while(1):
+			ret = ser.readline()
+			print("Command result: %s" % ret)
 			if ("OK" in ret.decode()):
 				break
 			if ("ERROR" in ret.decode()):
-				exit()
-		except UnicodeDecodeError :
-			print("error decoding")
+				raise Exception("write data error")
 
-def read_loop(resp=False):
-	while(1):
-		ret = ser.readline()
-		print("Command result: %s" % ret)
-		if resp:
+	def write_cmd(self, cmd):
+		ser = self.ser
+
+		ser.flushInput()
+		ser.write((cmd + "\r\n").encode())
+
+		return self.read_for_ok()
+
+	def read_for_ok(self):
+		ret = b''
+		while(1):
+			line = ser.readline()
+			ret += line
+			print("Command result: %s" % line)
 			try:
-				if ("IPD" in ret.decode()):
-					write_data("Got response :)\n", linkID=0)
+				if ("OK" in line.decode()):
+					break
+				if ("ERROR" in line.decode()):
+					raise Exception("cmd error")
 			except UnicodeDecodeError :
 				print("error decoding")
-			
+		return ret
 
-mod = "black"
+	# def read_loop(self, resp=False):
+	# 	ser = self.ser
+	#
+	# 	while(1):
+	# 		ret = ser.readline()
+	# 		print("Command result: %s" % ret)
+	# 		if resp:
+	# 			try:
+	# 				if ("IPD" in ret.decode()):
+	# 					write_data("Got response :)\n", linkID=0)
+	# 			except UnicodeDecodeError :
+	# 				print("error decoding")
 
-if mod == "blue":
-	port = "/dev/cu.SLAB_USBtoUART"
-	speed = "115200"
+	def find_connection(self):
+		# list access points
+		aps = write_cmd("AT+CWLAP").decode('utf-8', errors='ignore')
+		valid = espPat.findall(aps)
+		for ap in valid:
+			apId = ap[len("ESP8266-Mesh-"):]
+			try:
+				self.write_cmd("AT+CWJAP_CUR=\"ESP8266-Mesh-{}\",\"1234567890\"".format(apId))
+				# TODO fix link id
+				linkId = 1
+				self.write_cmd("AT+CIPSTART={},\"TCP\",\"192.168.{}.1\",80".format(linkId, apId))
+			except Exception:
+				continue
+			self.connection_ssid = ap
+			self.connected_to_ap = True
+			# TODO fix linkid and ip
+			ip = ""
+			add_conn(0, ap, ip)
+			# get connection ip
+			write_cmd("AT+CIFSR")
 
-	ser = serial.Serial(port,speed)
-	if ser.isOpen():
-		ser.close()
+	def run(self):
+		print("Running module thread")
+		module.setup()
+		module.find_connection()
+		# TODO maybe can get remote ip from here
+		self.write_cmd("AP+CIPSTATUS")
+		while(True):
+			line = ser.readline()
+			print("Read bytes: %s" % line)
+			# check for connection and disconnection messages
+			# send periodic sync messages to all clients
+			# read tcp buffer
+			# get link buffer lengths
+			# AT+CIPRECVLEN?
+			# get data from links
+			# AT+CIPRECVDATA=<link_id>,<len>
+			# 	ret: +CIPRECVDATA:<actual_len>,<data>
 
-	ser.open()
-	ser.isOpen()
-
-	write_cmd("AT")
-	write_cmd("AT")
-	# get info
-	write_cmd("AT+GMR")
-	# switch to station+AP mode
-	write_cmd("AT+CWMODE=3")
-	# set ssid (name), pwd, chnl, enc
-	write_cmd("AT+CWSAP_CUR=\"ESP8266-Blue\",\"1234567890\",5,3")
-	# list access points
-	write_cmd("AT+CWLAP")
-	# connect to other device
-	write_cmd("AT+CWJAP_CUR=\"ESP8266-Black\",\"1234567890\"")
-	# get connection ip
-	write_cmd("AT+CIFSR")
-
-	# single connection mode
-	write_cmd("AT+CIPMUX=0")
-
-	# connect to tcp server on port 80
-	write_cmd("AT+CIPSTART=\"TCP\",\"192.168.5.1\",80")
-	# write some data
-	write_data("abxy\n")
-	write_data("rrff55\n")
-	read_loop()
-else:
-	port = "/dev/cu.SLAB_USBtoUART13"
-	speed = "115200"
-
-	ser = serial.Serial(port,speed)
-	if ser.isOpen():
-		ser.close()
-
-	ser.open()
-	ser.isOpen()
-
-	write_cmd("AT")
-	write_cmd("AT")
-	# get info
-	write_cmd("AT+GMR")
-	# get info
-	# write_cmd("AT+UART_CUR?")
-	# switch to station+AP mode
-	write_cmd("AT+CWMODE=3")
-	# set name
-	write_cmd("AT+CWSAP_CUR=\"ESP8266-7\",\"1234567890\",5,3")
-	# set ip address
-	write_cmd("AT+CIPAP_CUR=\"192.168.7.1\",\"192.168.7.1\",\"255.255.255.0\"")
-	# allow multiple connections (necessary for tcp server)
-	write_cmd("AT+CIPMUX=1")
-	# active mode tcp receive
-	# write_cmd("AT+CIPRECVMODE=0")
-	# create a tcp server on port 80
-	write_cmd("AT+CIPSERVER=1,80")
-	# get local ip
-	write_cmd("AT+CIFSR")
-	# # listen from connections from remote ip 192.168.4.2
-	# # write_cmd("AT+CIPSTART=0,\"UDP\",\"192.168.4.2\",4445,4445,2")
-	read_loop(resp=True)
+id = 1
+for port in glob.glob("/dev/cu.SLAB_USBtoUART*"):
+	module = WifiModule(port,id)
+	module.start()
+	id+=1
