f93a063d10f2223a39624d525e65c8bd9eedad11
Daniel Weber <dantech88@yahoo.com>
2018-11-16 21:38:13 -0500

    ping works

--- mesh.py
+++ mesh.py
@@ -3,6 +3,7 @@ import re
 import glob
 import threading
 import time
+import datetime
 
 espPat = re.compile("ESP8266-Mesh-\d*")
 ipRE = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
@@ -16,7 +17,7 @@ class WifiModule(threading.Thread):
 		threading.Thread.__init__(self)
 		self.port = port
 		self.id = id
-		ser = serial.Serial(port,baud)
+		ser = serial.Serial(port,baud, timeout=5)
 		if ser.isOpen():
 			ser.close()
 
@@ -29,13 +30,18 @@ class WifiModule(threading.Thread):
 		self.direct_conns = {}
 
 		self.connected_to_ap = False
+		self.to_ping = []
 
 	def iprint(self, str):
-		print("{}: {}".format(self.port, str))
+		ts = datetime.datetime.now().timestamp()
+		now = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+		print("{}: {}: {}".format(now, self.port, str))
 
-	def add_conn(self, linkId, mac, IP):
-		self.iprint("Adding connection: {} {} {}".format(linkId, mac, IP))
-		self.direct_conns[linkId] = (linkId, mac, IP)
+	# Type is 0 if this is a station connected to my AP
+	# and a 1 if this is an AP that my station is connected to
+	def add_conn(self, type, linkId, mac, IP):
+		self.iprint("Adding connection: {} {} {} {}".format(type, linkId, mac, IP))
+		self.direct_conns[linkId] = (type, linkId, mac, IP)
 
 	def reserve_conn(self, linkId):
 		self.iprint("Reserving connection: {}".format(linkId))
@@ -60,8 +66,12 @@ class WifiModule(threading.Thread):
 		self.write_cmd("AT+GMR")
 		# switch to station+AP mode
 		self.write_cmd("AT+CWMODE=3")
+		# get mac
+		result = safe_dec(self.write_cmd("AT+CIPSTAMAC_CUR?"))
+		self.mac = macRE.findall(result)[0]
 		# set ip address to avoid conflicts
-		self.write_cmd("AT+CIPAP_CUR=\"192.168.{}.1\",\"192.168.{}.1\",\"255.255.255.0\"".format(self.id, self.id))
+		self.ip = "192.168.{}.1".format(self.id)
+		self.write_cmd("AT+CIPAP_CUR=\"192.168.{}.1\",\"{}\",\"255.255.255.0\"".format(self.id, self.ip))
 		# allow multiple connections (necessary for tcp server)
 		self.write_cmd("AT+CIPMUX=1")
 
@@ -122,52 +132,63 @@ class WifiModule(threading.Thread):
 				self.iprint("error decoding")
 		return ret
 
-	# def read_loop(self, resp=False):
-	# 	ser = self.ser
-	#
-	# 	while(1):
-	# 		ret = self.read_line()
-	# 		self.iprint("Command result: %s" % ret)
-	# 		if resp:
-	# 			try:
-	# 				if ("IPD" in ret.decode()):
-	# 					write_data("Got response :)\n", linkId=0)
-	# 			except UnicodeDecodeError :
-	# 				self.iprint("error decoding
-
 	def find_connection(self):
 		# list access points
 		aps = safe_dec(self.write_cmd("AT+CWLAP"))
 		valid = espPat.findall(aps)
 		for ap in valid:
 			apId = ap[len("ESP8266-Mesh-"):]
-			# try:
-			self.write_cmd("AT+CWJAP_CUR=\"ESP8266-Mesh-{}\",\"1234567890\"".format(apId))
-			linkId = self.get_next_linkId()
-			self.write_cmd("AT+CIPSTART={},\"TCP\",\"192.168.{}.1\",80".format(linkId, apId))
+			try:
+				self.write_cmd("AT+CWJAP_CUR=\"ESP8266-Mesh-{}\",\"1234567890\"".format(apId))
+				linkId = self.get_next_linkId()
+				self.write_cmd("AT+CIPSTART={},\"TCP\",\"192.168.{}.1\",80".format(linkId, apId))
 
-			self.connection_ssid = ap
-			self.connected_to_ap = True
-			self.add_conn(linkId, ap, "192.168.{}.1".format(apId))
-			# get connection ip
-			result = safe_dec(self.write_cmd("AT+CIFSR")).split("\r\n")
-			for result_line in result:
-				if result_line.startswith("+CIFSR:STAIP"):
-					ip = ipRE.findall(result_line)[0]
-				elif result_line.startswith("+CIFSR:STAMAC"):
-					mac = macRE.findall(result_line)[0]
-			if ip and mac:
-				self.write_data(linkId, "C,{},{}\n".format(ip, mac))
-			# self.write_cmd("AT+CIPSTATUS")
-			break
-			# except Exception as e:
-			# 	self.iprint("There was an exception")
-			# 	continue
+				self.connection_ssid = ap
+				self.connected_to_ap = True
+				# self.add_conn(linkId, ap, "192.168.{}.1".format(apId))
+				# get connection ip
+				result = safe_dec(self.write_cmd("AT+CIFSR")).split("\r\n")
+				for result_line in result:
+					if result_line.startswith("+CIFSR:STAIP"):
+						ip = ipRE.findall(result_line)[0]
+					elif result_line.startswith("+CIFSR:STAMAC"):
+						mac = macRE.findall(result_line)[0]
+				if ip and mac:
+					# send the AP your information
+					self.write_data(linkId, "CS,{},{}\n".format(mac, ip))
+				break
+			except Exception as e:
+				self.iprint("There was an exception")
+				continue
 
 		if self.connected_to_ap:
 			self.iprint("Connected to AP")
 		else:
 			self.iprint("Unable to find mesh AP")
+
+	def process_data(self, linkId, data):
+		packets = data.split(",")
+		if len(packets) < 1:
+			return
+
+		print(packets)
+		# connection confirmation from station
+		if packets[0] == "CS":
+			self.add_conn(0, linkId, packets[1], packets[2])
+			# self.write_cmd("AT+CWLIF")
+			self.write_data(linkId, "\n")
+			self.write_data(linkId, "CA,{},{}\n".format(self.mac, self.ip))
+		# connection confirmation from AP
+		elif packets[0] == "CA":
+			self.add_conn(1, linkId, packets[1], packets[2])
+			self.write_data(linkId, "\n")
+			self.ping(linkId)
+		# ping packet to keep connection alive
+		elif packets[0] == "P":
+			self.to_ping.append((linkId,datetime.datetime.now().timestamp()+int(packets[1])))
+
+	def ping(self, linkId):
+		self.write_data(linkId, "P,5\n")
 
 	def process_line(self, line):
 		if line.startswith("+STA_CONNECTED"):
@@ -178,8 +199,10 @@ class WifiModule(threading.Thread):
 			idx3 = line.index(":", idx2+1)
 			linkId = int(line[idx1+1:idx2])
 			dataLen = int(line[idx2+1:idx3])
-			data = line[idx3:idx3+dataLen]
-			self.iprint("got data for {} of len {}".format(linkId, dataLen))
+			# strip newline
+			data = line[idx3+1:idx3+dataLen]
+			self.process_data(linkId, data)
+			# self.iprint("got data for {} of len {}".format(linkId, dataLen))
 		elif line.startswith("+DIST_STA_IP"):
 			pass
 		elif line.startswith("+LINK_CONN"):
@@ -209,39 +232,17 @@ class WifiModule(threading.Thread):
 		while(True):
 			line = self.read_line()
 			self.iprint("Read bytes: %s" % line)
-			# if safe_dec(line).startswith("+STA_CONNECTED:"):
-			# 	# get assigned IP
-			# 	line = self.read_line()
-			# 	self.iprint("Read bytes: %s" % line)
-			# 	line = safe_dec(line)
-			# 	if line.startswith("+DIST_STA_IP:"):
-			# 		# TODO could throw error
-			# 		ip = ipRE.findall(line)[0]
-			#
-			# 		# get linkId
-			# 		line = self.read_line()
-			# 		self.iprint("Read bytes: %s" % line)
-			# 		line = safe_dec(line)
-			# 		if line.endswith(",CONNECT\r\n"):
-			# 			linkId = int(line[:-len(",CONNECT\r\n")])
-			# 			self.add_conn(linkId, "", ip)
-			#
-			# 			self.write_data(linkId, "Hello from the AP\n")
-				# self.write_cmd("AT+CIPSTATUS")
-			# check for connection and disconnection messages
-			# send periodic sync messages to all clients
-			# read tcp buffer
-			# get link buffer lengths
-			# AT+CIPRECVLEN?
-			# get data from links
-			# AT+CIPRECVDATA=<link_id>,<len>
-			# 	ret: +CIPRECVDATA:<actual_len>,<data>
-			# On connection:
-			# 	+STA_CONNECTED:"98:01:a7:a2:2b:53"\r\n'
-			# Assigned IP:
-			#	+DIST_STA_IP:"98:01:a7:a2:2b:53","192.168.1.2"\r\n'
-			# Disconnected:
-			#	+STA_DISCONNECTED:"98:01:a7:a2:2b:53"\r\n
+
+			# figure out if I have to ping someone
+			too_early = []
+			while self.to_ping:
+				(toPingLink, timeToPing) = self.to_ping.pop()
+				if timeToPing < datetime.datetime.now().timestamp():
+					self.ping(toPingLink)
+				else:
+					too_early.append((toPingLink, timeToPing))
+			for elt in too_early:
+				self.to_ping.append(elt)
 
 id = 1
 for port in glob.glob("/dev/cu.SLAB_USBtoUART*"):
